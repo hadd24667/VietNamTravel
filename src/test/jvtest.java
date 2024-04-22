db31e8f5519d3c1481affd831ed08c304dfbdfa3
hieuVKU <157434211+hieuVKU@users.noreply.github.com>
2024-04-22 11:38:29 +0700

    update2

--- src/test/jvtest.java
+++ src/test/jvtest.java
@@ -3,5 +3,6 @@ package test;
 public class jvtest {
  public static void main(String[] args) {
 	System.out.println("Hello con card");
+     System.out.println("con card: hiiiiiiiiiiiiiiiiiiiiiii");
 }
 }
