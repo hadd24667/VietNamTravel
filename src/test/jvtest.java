28d57fd6bdbbcddc0e0c420187f46ca3413b429c
hieuVKU <hieutv.23itb@vku.udn.vn>
2024-04-22 21:23:36 +0700

    update vòng lặp

--- src/test/jvtest.java
+++ src/test/jvtest.java
@@ -4,5 +4,8 @@ public class jvtest {
  public static void main(String[] args) {
 	System.out.println("Hello con card");
      System.out.println("con card: hiiiiiiiiiiiiiiiiiiiiiii");
+     for(int i=0;i<9;i++){
+         System.out.print(i+" ");
+     }
 }
 }
