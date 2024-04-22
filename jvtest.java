9f2b5b006cfd53ed98d44b1bb9cd008285d4de81
hieuVKU <157434211+hieuVKU@users.noreply.github.com>
2024-04-22 11:06:27 +0700

    Update2

--- jvtest.java
+++ jvtest.java
@@ -1,8 +1,1 @@
-package test;
 
-public class jvtest {
- public static void main(String[] args) {
-	System.out.println("Hello con card");
-     System.out.println("con card: hiiiiiiiiiiiiiiiiiiiiiii");
-}
-}
